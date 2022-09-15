bfa5acca8a19f5d85d46d2b581278be0d5a6bf4f
Ivan Muñoz <ivandariomunioz@gmail.com>
2022-09-15 07:45:38 -0300

    fin pg admin - comienzo panel admin pag22

--- sistemaEmpleadosConcremix/gestionEmpleados/models.py
+++ sistemaEmpleadosConcremix/gestionEmpleados/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Empleados(models.Model):
     apellido_nombre=models.CharField(max_length=255)
     foto=models.ImageField()
-    fecha_nac=models.DateField()
+    fecha_nac=models.DateField() # 'aaaa-mm-dd'
     dni=models.CharField(max_length=20)
     telefono=models.CharField(max_length=20)
     correo=models.EmailField()
@@ -11,6 +11,8 @@ class Empleados(models.Model):
     puesto=models.CharField(max_length=50)
     vehiculo=models.CharField(max_length=100)
     recinto=models.CharField(max_length=100)
+    def __str__(self):
+        return 'El empleado es: %s, dni: %s, telefono: %s, correo: %s, puesto: %s, recinto: %s'%(self.apellido_nombre,self.dni,self.telefono,self.correo,self.puesto,self.recinto)
 
 class Vehiculos(models.Model):
     nombre_vehiculo=models.CharField(max_length=100)
