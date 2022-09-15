70bf7910abfd7232df28dffdd9652ec3b29ddcc6
Ivan Muñoz <ivandariomunioz@gmail.com>
2022-09-15 15:14:56 -0300

    settings,gestionEmpleados: admin, models

--- sistemaEmpleadosConcremix/gestionEmpleados/admin.py
+++ sistemaEmpleadosConcremix/gestionEmpleados/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from gestionEmpleados.models import Empleados,Vehiculos,Recintos
 
-# Register your models here.
+class EmpleadosAdmin(admin.ModelAdmin):
+    list_display = ("apellido_nombre","foto","fecha_nac","dni","telefono","correo","direccion","puesto","vehiculo","recinto",)
+    search_fields = ("apellido_nombre","dni","puesto","vehiculo","recinto",)
+    list_filter = ("puesto","vehiculo","recinto",)
+
+class VehiculosAdmin(admin.ModelAdmin):
+    list_display = ("nombre_vehiculo","marca","modelo","interno",)
+    search_fields = ("interno","modelo",)
+    list_filter = ("modelo","marca",)
+
+class RecintosAdmin(admin.ModelAdmin):
+    list_display = ("nombre_recinto",) # si tiene un solo item debe agregar la coma al final para q lo considere tupla
+    search_fields = ("nombre_recinto",)
+
+admin.site.register(Empleados,EmpleadosAdmin)
+admin.site.register(Vehiculos,VehiculosAdmin)
+admin.site.register(Recintos,RecintosAdmin)
--- sistemaEmpleadosConcremix/gestionEmpleados/models.py
+++ sistemaEmpleadosConcremix/gestionEmpleados/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 class Empleados(models.Model):
-    apellido_nombre=models.CharField(max_length=255)
-    foto=models.ImageField()
-    fecha_nac=models.DateField() # 'aaaa-mm-dd'
-    dni=models.CharField(max_length=20)
+    apellido_nombre=models.CharField(max_length=255,verbose_name="Nombre completo")
+    foto=models.ImageField(blank=True,null=True)
+    fecha_nac=models.DateField(verbose_name="Fecha nacimiento") # 'aaaa-mm-dd'
+    dni=models.CharField(max_length=20,verbose_name="DNI")
     telefono=models.CharField(max_length=20)
-    correo=models.EmailField()
+    correo=models.EmailField(blank=True,null=True)
     direccion=models.CharField(max_length=255)
     puesto=models.CharField(max_length=50)
-    vehiculo=models.CharField(max_length=100)
-    recinto=models.CharField(max_length=100)
+    vehiculo=models.CharField(max_length=100,blank=True,null=True)
+    recinto=models.CharField(max_length=100,blank=True,null=True)
     def __str__(self):
         return 'El empleado es: %s, dni: %s, telefono: %s, correo: %s, puesto: %s, recinto: %s'%(self.apellido_nombre,self.dni,self.telefono,self.correo,self.puesto,self.recinto)
 
 class Vehiculos(models.Model):
-    nombre_vehiculo=models.CharField(max_length=100)
+    nombre_vehiculo=models.CharField(max_length=100,verbose_name="Nombre vehiculo")
     marca=models.CharField(max_length=100)
     modelo=models.IntegerField()
     interno=models.IntegerField()
