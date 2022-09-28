9f3c4e0527d5725be62c6cc15d24270ab65aed4b
Ivan Muñoz <ivandariomunioz@gmail.com>
2022-09-28 13:58:15 -0300

    arreglos en gestionEmpleados: empleados y vehiculos -> listos. Falta recintos todo!

--- sistemaEmpleadosConcremix/gestionEmpleados/views.py
+++ sistemaEmpleadosConcremix/gestionEmpleados/views.py
@@ -14,12 +14,30 @@ def crear_empleado(request):
         form_empleado = FormuCrearEmpleado(request.POST,request.FILES)
         if form_empleado.is_valid():
             empleado = Empleado.objects.create(**form_empleado.cleaned_data)
+            messages.success(request,"Empleado cargado satisfactoriamente.")
         else:
-            raise ValueError("formulario no valido")
+            messages.error(request,"Datos del formulario inválidos")
         return redirect('Empleados')
     else:
         return render(request, "gestionEmpleados/crear_empleado.html", {"fecha":fecha_actual,"agno":anio}) # GET. me sirve el form para crear/ingresar empleado
-        
+
+def crear_vehiculo(request):
+    fecha_actual=datetime.datetime.now().strftime("%D - %H:%M:%S")
+    anio=datetime.datetime.now().strftime("%Y")
+    form_vehiculo = FormuCrearVehiculo()
+    if request.method == 'POST':
+        form_vehiculo = FormuCrearVehiculo(request.POST) # como hago para no repetir esto?
+        if form_vehiculo.is_valid():
+            vehiculo = Vehiculo.objects.create(**form_vehiculo.cleaned_data) # hace falta la variable realmente???
+            messages.success(request,"Vehículo cargado correctamente")
+        else:
+            messages.error(request,"Datos de formulario incorrectos")
+        return redirect('Vehiculos')
+    else:
+        return render(request, "gestionEmpleados/crear_vehiculo.html", {"fecha":fecha_actual,"agno":anio,"form_vehiculo":form_vehiculo}) # me sirve el form para crear/ingresar empleado
+    
+
+
 def editar_empleado(request,id):
     id_empleado= id
     empleado = Empleado.objects.filter(id=id_empleado)
@@ -27,9 +45,10 @@ def editar_empleado(request,id):
         form_empleado = FormuCrearEmpleado(request.POST,request.FILES)
         if form_empleado.is_valid():
             empleado.update(**form_empleado.cleaned_data)
+            messages.success(request,"Empleado actualizado satisfactoriamente")
         else:
-            print(form_empleado.errors)
-            raise ValueError("formulario no valido")
+            print(form_empleado.errors) # esto en produccion se va...
+            messages.error(request,"Datos incorrectos en formulario")
         return redirect('Empleados')
     else:
         fecha_actual=datetime.datetime.now().strftime("%D - %H:%M:%S")
@@ -47,6 +66,7 @@ def alta_baja(request,id):
         empleado.fecha_ingreso = datetime.datetime.now()
         empleado.activo = True
     empleado.save()
+    messages.info(request,"Estado modificado con éxito")
     return redirect('Empleados')
 
 def planilla_empleados(request):
@@ -58,18 +78,17 @@ def planilla_empleados(request):
 def busqueda_empleados(request):
     fecha_actual=datetime.datetime.now().strftime("%D - %H:%M:%S")
     anio=datetime.datetime.now().strftime("%Y")
+    if request.method == 'POST':
+        if request.POST["emp"]:
+            nombre_empleado= request.POST["emp"]
+            empleados=Empleado.objects.filter(apellido_nombre__icontains=nombre_empleado).order_by('apellido_nombre')
+            messages.info(request,"Búsqueda realizada")
+            return render(request,"gestionEmpleados/resultados_busqueda_empleado.html",{"empleados":empleados,"query":nombre_empleado,"fecha":fecha_actual,"agno":anio})
+        else:
+            messages.warning(request,"No has introducido ningún dato")
+            return redirect('Empleados')
     return render(request, "gestionEmpleados/busqueda_empleados.html", {"fecha":fecha_actual,"agno":anio})
 
-def buscar_empleado(request):
-    fecha_actual=datetime.datetime.now().strftime("%D - %H:%M:%S")
-    anio=datetime.datetime.now().strftime("%Y")
-    if request.GET["emp"]:
-        nombre_empleado= request.GET["emp"]
-        empleados=Empleado.objects.filter(apellido_nombre__icontains=nombre_empleado).order_by('apellido_nombre')
-        return render(request,"gestionEmpleados/resultados_busqueda_empleado.html",{"empleados":empleados,"query":nombre_empleado,"fecha":fecha_actual,"agno":anio})
-    else:
-        mensaje= "No has introducido ningún dato."
-        return HttpResponse(mensaje)
 
 # VEHICULOS
 def planilla_vehiculos(request):
@@ -81,14 +100,16 @@ def planilla_vehiculos(request):
 def busqueda_vehiculos(request):
     fecha_actual=datetime.datetime.now().strftime("%D - %H:%M:%S")
     anio=datetime.datetime.now().strftime("%Y")
-    if request.method == 'GET':
-        if request.GET["vehi"]:
-            nombre_vehiculo= request.GET["vehi"]
+    if request.method == 'POST':
+        if request.POST["vehi"]:
+            nombre_vehiculo= request.POST["vehi"]
             vehiculos=Vehiculo.objects.filter(nombre_vehiculo__icontains=nombre_vehiculo).order_by('interno')
+            messages.info(request,"Búsqueda realizada")
             return render(request,"gestionEmpleados/resultados_busqueda_vehiculo.html",{"vehiculos":vehiculos,"query":nombre_vehiculo,"fecha":fecha_actual,"agno":anio})
         else:
-            mensaje= "No has introducido ningún dato."
-            return HttpResponse(mensaje)
+            messages.warning(request,"No has introducido ningún dato")
+            return redirect('Vehiculos')
+            
     else:
         return render(request, "gestionEmpleados/busqueda_vehiculos.html", {"fecha":fecha_actual,"agno":anio})
 
@@ -100,8 +121,9 @@ def crear_vehiculo(request):
         form_vehiculo = FormuCrearVehiculo(request.POST) # como hago para no repetir esto?
         if form_vehiculo.is_valid():
             vehiculo = Vehiculo.objects.create(**form_vehiculo.cleaned_data) # hace falta la variable realmente???
+            messages.success(request,"Vehículo cargado correctamente")
         else:
-            raise ValueError("formulario no valido")
+            messages.error(request,"Datos de formulario incorrectos")
         return redirect('Vehiculos')
     else:
         return render(request, "gestionEmpleados/crear_vehiculo.html", {"fecha":fecha_actual,"agno":anio,"form_vehiculo":form_vehiculo}) # me sirve el form para crear/ingresar empleado
@@ -116,7 +138,7 @@ def editar_vehiculo(request,id):
             vehiculo.update(**form_vehiculo.cleaned_data)
             messages.success(request,"Vehículo actualizado correctamente")
         else:
-            print(form_vehiculo.errors)
+            print(form_vehiculo.errors) # en produccion esto se va
             messages.error(request,"Algún dato del formulario es incorrecto")
         return redirect('Vehiculos')
     else:
@@ -125,8 +147,7 @@ def editar_vehiculo(request,id):
         return render(request, "gestionEmpleados/editar_vehiculo.html", {"form_vehiculo":form_vehiculo,"vehiculo":vehiculo,"fecha":fecha_actual,"agno":anio})
 
 def eliminar_vehiculo(request,id):
-    id_vehiculo = id
-    vehiculo = Vehiculo.objects.get(id=id_vehiculo)
+    vehiculo = Vehiculo.objects.get(id=id)
     vehiculo.delete()
     messages.warning(request,"Vehículo eliminado")
     return redirect('Vehiculos')
